41d4d9d1e8bd503e4f148fb95e40be12a8275218
Kayro França <k4yro01@gmail.com>
2020-12-12 20:59:29 -0300

    added js functions modal

---
+++ index.js
@@ -0,0 +1,11 @@
+function popup() {
+    const box = document.getElementById("popupRules");
+
+    box.classList.add('show');
+
+    box.addEventListener('click', (event) => {
+        if (event.target.id == 'popupRules' || event.target.id == 'closePopup'){
+            box.classList.remove('show');
+        }
+    });
+}
